16348a3da0b0cbaa5b72ae8aa36cd6fd914084f8
Vasant Hugar <vasant.hugar@indpro.se>
2017-11-10 11:30:30 +0530

    Initial Commit

---
+++ VNHCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VNHCoreData
+//
+//  Created by Vasant Hugar on 10/11/17.
+//  Copyright © 2017 Vasant Hugar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
